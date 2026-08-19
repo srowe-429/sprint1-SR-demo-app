ceb62e28209397342ff61f479af660b5734e8ade
srowe-429 <s.rowe429@gmail.com>
2026-08-19 18:09:04 +0000

    adding more files

--- src/main/java/com/neueda/leap/Greeter.java
+++ src/main/java/com/neueda/leap/Greeter.java
@@ -1,7 +1,7 @@
+package com.neueda.leap;
+
 public class Greeter {
     public String greet(String name) {
-        // Hello -Austin
-
         return "Good day, " + name;
     }
 }
--- starter/README.md
+++
@@ -1,3 +0,0 @@
-# Sprint 1 Demo App
-
-A tiny placeholder project used to practise GitHub essentials. Continues from Modules 06-07.
